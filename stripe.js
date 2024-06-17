c9d3f39b548519f3e382a3ba6e7881603d451ac4
Docbsk <108417086+Alby9z@users.noreply.github.com>
2024-06-17 17:40:49 +0200

    page-paiement

--- stripe.js
+++ stripe.js
@@ -14,14 +14,15 @@ app.use(bodyParser.json());
 // Servir les fichiers statiques depuis le répertoire 'public'
 app.use(express.static(path.join(__dirname, "digiprogtech")));
 
-// Route pour créer une intention de paiement
+// Routes pour créer une intention de paiement
 app.post("/create-payment-intent", async (req, res) => {
-  const { amount } = req.body;
+  const { amount, pack } = req.body;
 
   try {
+    // Créer une intention de paiement avec Stripe
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
-      currency: "€",
+      currency: "eur", // Devise en euros
     });
 
     res.status(200).send({
@@ -32,7 +33,35 @@ app.post("/create-payment-intent", async (req, res) => {
   }
 });
 
-// Configurer le port d'écoute
+// Route pour télécharger le fichier PDF en fonction du pack choisi
+app.get("/download-pdf", (req, res) => {
+  const { pack } = req.query;
+
+  // Déterminer le chemin du fichier PDF en fonction du pack
+  let filePath;
+  switch (pack) {
+    case "pack-autonome":
+      filePath = path.join(__dirname, "pdfs", "pack_autonome.pdf");
+      break;
+    case "creation-personnalisee":
+      filePath = path.join(__dirname, "pdfs", "creation_personnalisee.pdf");
+      break;
+    case "creation-complete":
+      filePath = path.join(__dirname, "pdfs", "creation_complete.pdf");
+      break;
+    default:
+      return res.status(404).send("Pack not found");
+  }
+
+  // Vérifier si le fichier existe
+  if (fs.existsSync(filePath)) {
+    // Envoyer le fichier PDF en réponse à la demande
+    res.download(filePath);
+  } else {
+    res.status(404).send("File not found");
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
